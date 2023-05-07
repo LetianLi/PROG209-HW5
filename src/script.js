c97adff7c62245f0a240892b8f3e21e9de14b797
LetianLi <letian_li@outlook.com>
2023-05-06 21:56:20 -0700

    implement sorting and filtering

--- src/script.js
+++ src/script.js
@@ -1,10 +1,33 @@
 let tasks = [];
 
+let Task = function(taskName, taskType, taskPriority) {
+    this.name = taskName;
+    this.type = taskType;
+    this.priority = taskPriority;
+
+    if (this.priority === "High") this.priorityNumber = 1;
+    else if (this.priority === "Medium") this.priorityNumber = 2;
+    else this.priorityNumber = 3;
+}
+
+// prepush tasks
+tasks.push(new Task("Apply for social security", "Life", "High"));
+tasks.push(new Task("Buy groceries", "Life", "Low"));
+tasks.push(new Task("Check for response from support ticket", "Work", "High"));
+tasks.push(new Task("Complain about pay", "Work", "Low"));
+tasks.push(new Task("Do Homework", "School", "Medium"));
+tasks.push(new Task("Review class notes", "School", "Medium"));
+tasks.push(new Task("Talk to coworker", "Work", "Low"));
+
 document.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM loaded");
     document.getElementById("getStartedBtn").addEventListener("click", goToAddPage);
     document.getElementById("getStartedBtn2").addEventListener("click", goToAddPage);
     document.getElementById("addTaskBtn").addEventListener("click", createArrayObj);
+
+    // regenerate table whenever sort/filter settings change
+    let regenDisplayElements = Array.from(document.getElementsByClassName("regenDisplay"));
+    regenDisplayElements.forEach(element => element.addEventListener("change", displayTasks));
 });
 
 $(document).on("pagebeforeshow", "#DisplayPage", function (event) { 
@@ -20,11 +43,7 @@ function createArrayObj() {
     let taskType = document.getElementById("taskTypeInput").value;
     let taskPriority = document.getElementById("taskPriorityInput").value;
     //task array
-    let task = {
-        name: taskName,
-        type: taskType,
-        priority: taskPriority,
-    };
+    let task = new Task(taskName, taskType, taskPriority);
     tasks.push(task);
 
     /*displays task added successfully so I know it actually
@@ -36,7 +55,8 @@ function createArrayObj() {
 function displayTasks() {
     if (tasks.length > 0) {
         // create table and hide button
-        createTaskTable();
+        let processedTaskList = processTaskList();
+        createTaskTable(processedTaskList);
         document.getElementById("getStartedBtn2").style = "display: none";
     } else {
         // replace table with empty message and show button
@@ -45,7 +65,47 @@ function displayTasks() {
     }
 }
 
-function createTaskTable() {
+function processTaskList() {
+    // Filter task list
+    let allowedTypes = Array.from(document.getElementsByClassName("filterTypeSetting")).filter(checkbox => checkbox.checked).map(checkedBoxes => checkedBoxes.value);
+    let allowedPriorities = Array.from(document.getElementsByClassName("filterPrioritySetting")).filter(checkbox => checkbox.checked).map(checkedBoxes => checkedBoxes.value);
+    
+    let filteredTasks = tasks.filter(task => {
+        let passesTypeCheck = allowedTypes.length === 0 || allowedTypes.includes(task.type);
+        let passesPriorityCheck = allowedPriorities.length === 0 || allowedPriorities.includes(task.priority);
+
+        console.log(task);
+        console.log("Passes type: " + passesTypeCheck);
+        console.log("Passes priority: " + passesPriorityCheck);
+        return passesTypeCheck && passesPriorityCheck;
+    });
+
+    // Sort task list
+    let sortProperty = document.getElementById("sortSetting").value;
+    let sortAscending = document.getElementById("sortAscendingCheckBox").checked;
+    let sortedTasks = Array.from(filteredTasks);
+    sortedTasks.sort((taskA, taskB) => {
+        let propA = taskA[sortProperty];
+        let propB = taskB[sortProperty];
+        
+        let sortValue;
+        if (typeof propA === "string") {
+            sortValue = propA.localeCompare(propB);
+        } else if (typeof propA === "number") {
+            sortValue = propA - propB;
+        }
+
+        if (sortAscending) {
+            return sortValue;
+        } else {
+            return -sortValue;
+        }
+    });
+
+    return sortedTasks;
+}
+
+function createTaskTable(processedTaskList) {
     let table = document.getElementById("table");
 
     //to Clears the table
@@ -73,8 +133,7 @@ function createTaskTable() {
     table.appendChild(headerRow);
 
     //setting up rows for each task item in array
-    for (let i = 0; i < tasks.length; i++) {
-        let task = tasks[i];
+    processedTaskList.forEach(task => {
         let row = document.createElement("tr");
 
         //for name col
@@ -94,5 +153,5 @@ function createTaskTable() {
 
 
         table.appendChild(row);
-    }
+    });
 }
